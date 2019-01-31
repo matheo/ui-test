2350c4d806904ceea235b378736db100ab7bffc7
Mateo Tibaquirá <nestormateo@gmail.com>
2019-01-30 22:34:02 -0500

    feat(frontend): archive and mechanics modules added

---
+++ apps/frontend/src/app/mechanics/mechanics.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MechanicsRoutingModule } from './mechanics-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MechanicsRoutingModule
+  ]
+})
+export class MechanicsModule { }
